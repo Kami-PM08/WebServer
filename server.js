2a8d5ff5e82ec35e2f961acf50c3d68a63daf912
Kami-PM08 <37862210+Kami-PM08@users.noreply.github.com>
2020-08-25 16:56:37 -0500

    Final commit

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     // res.send('Hola Mundo');
-//     let salida = {
-//         nombre: 'Cristian',
-//         edad: 20,
-//         url: req.url
-//     }
-
-//     res.send(salida);
-// });
-// app.get('/data', (req, res) => {
-//     res.send('Hola Data');
+hbs.registerPartials(__dirname + '/views/partials');
 
 
-//     res.send(salida);
-// });
+app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000.');
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        name: "caMilo",
+    });
+});
+app.get('/about', (req, res) => {
+
+    res.render('about', {});
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}.`);
 })
